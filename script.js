b68917d406b6fe24071babbb9d2c5743227ef64b
yazanalkherret <186625651+yazanalkherret@users.noreply.github.com>
2024-12-28 22:29:21 +0300

    Allow user input to change number of cells in grid
    Add a button on the top of the screen that will
    send the user a popup asking for the number of
    squares per side for the new grid.
    
    Once entered, the existing grid will be removed,
    and a new grid will be generated in the same
    total space as before so that you've got a
    new sketch pad.

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 let gridSize = 16;
-let cellSize = 50;
 let cells = [];
 
-//Add cells to grid
 const grid = document.querySelector("#grid");
-let gridLength = gridSize * (cellSize + 2);
-grid.style["height"] = gridLength + "px";
-grid.style["width"] = gridLength + "px";
 
-for(let i = 0; i < gridSize; i++){
-    cells[i] = [];
-
-    for(let j = 0; j < gridSize; j++){
-        cells[i][j] = document.createElement("div");
-        cells[i][j].setAttribute("class", "cell");
-        cells[i][j].style["height"] = cellSize + "px";
-        cells[i][j].style["width"] = cellSize + "px";        
-        grid.appendChild(cells[i][j]);
+function drawGrid() {
+    //To remove all child elements (necessary when redrawing graph)
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
+    // calc cell size:
+    let cellSize = (960 / gridSize) - 2;
 
+    for (let i = 0; i < gridSize; i++) {
+        cells[i] = [];
+
+        for (let j = 0; j < gridSize; j++) {
+            cells[i][j] = document.createElement("div");
+            cells[i][j].setAttribute("class", "cell");
+            cells[i][j].style["height"] = cellSize + "px";
+            cells[i][j].style["width"] = cellSize + "px";
+            grid.appendChild(cells[i][j]);
+        }
+    }
 }
 
-grid.addEventListener("mouseover", (event)=>{
+function getUserEntry() {
+    let userEntry = window.prompt("Enter number of cells:\nMax 100");
+    if (userEntry > 0 && userEntry <= 100) {
+        //valid
+        gridSize = Number(userEntry);
+        drawGrid();
+    }
+    else {
+        alert("Invalid input");
+    }
+}
+
+grid.addEventListener("mouseover", (event) => {
     event.target.classList.add("hover");
 });
+
+const userBtn = document.querySelector("button");
+userBtn.addEventListener("click", () =>{
+    getUserEntry();
+});
+
+
+drawGrid();
